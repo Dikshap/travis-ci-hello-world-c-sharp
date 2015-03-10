32956ee249d6ef4cbf10c053d17c213e1427b70d
James Prankard <prankard@gmail.com>
2015-03-10 23:46:46 +0000

    Initial commit

---
+++ src/prankard/travisci/PassFail.cs
@@ -0,0 +1,21 @@
+using System;
+
+namespace prankard.travisci
+{
+	public class PassFail
+	{
+		public PassFail()
+		{
+		}
+
+		public bool Pass()
+		{
+			return true;
+		}
+
+		public bool Fail()
+		{
+			return false;
+		}
+	}
+}
---
+++ test/prankard/travisci/PassFailTest.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using NUnit.Framework;
+
+namespace prankard.travisci
+{
+	[TestFixture]
+	public class PassFailTest
+	{
+		public PassFail passFail;
+
+		[SetUp]
+		public void SetUp()
+		{
+			passFail = new PassFail();
+		}
+
+		[Test]
+		public void test_pass_returns_true()
+		{
+			Assert.That (passFail.Pass(), Is.True);
+		}
+
+		[Test]
+		public void test_fail_returns_false()
+		{
+			Assert.That (passFail.Fail (), Is.False);
+		}
+	}
+}
+
